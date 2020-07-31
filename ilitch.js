7fc6058ccbf8043aeb9ff169862e6342e6369fd4
zeroschool <68612067+zeroschool@users.noreply.github.com>
2020-07-31 17:38:15 +0200

    Update ilitch.js

--- ilitch.js
+++ ilitch.js
@@ -154,7 +154,7 @@ async function postsQuery() {
         orderBy = 'orderBy: LIKES_BY_POST_ID__COUNT_DESC'
     };
     let response = await sdk.query(`{
-                allPosts(filter: {bContent: {includes: `${twetchSuffix}`}}, ${selOrder === '2' ? "" : "first: 100,"} ${orderBy}) {
+                allPosts(filter: {bContent: {includes: ${twetchSuffix}}}, ${selOrder === '2' ? "" : "first: 100,"} ${orderBy}) {
                     nodes {bContent transaction numLikes userId youLiked userByUserId {name icon}}
                 }
             }`);

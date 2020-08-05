a88b59b8214f473345e49ac8414965927ffd4f44
zeroschool <68612067+zeroschool@users.noreply.github.com>
2020-08-05 11:49:13 +0200

    Update ilitch.js

--- ilitch.js
+++ ilitch.js
@@ -150,7 +150,7 @@ async function postsQuery() {
         let content = posts[i].bContent.replace(getTwetchSuffix(), '');
         let boostValue = diffSum(posts[i].transaction);
         posts[i].boostValue = boostValue;
-        let osTwetch = `<div onclick="goToTwetch()" class="nes-container with-title is-dark" style="position: relative; border-color: #777; background-color: #000000; margin-bottom: 20px;">
+        let osTwetch = `<div onclick="window.location.href='https://twetch.app/t/'+${posts[i].transaction}" class="nes-container with-title is-dark" style="position: relative; border-color: #777; background-color: #000000; margin-bottom: 20px;">
                         <p id="postTitle" class="title"><img class="nes-avatar is-rounded is-medium" src="${posts[i].userByUserId.icon}"> ${posts[i].userByUserId.name} <a href="https://twetch.app/u/${posts[i].userId}" target="_blank">u/${posts[i].userId}</a>
                         </p><p class="urlFormat">${applyURLs(content)}</p>`
         osTwetch += `<div class="item" style="position: relative; height: 110px;">
@@ -313,10 +313,6 @@ var dots = window.setInterval(function() {
         }
     }
 }, 300);
-
-function goToTwetch(){
-        window.location.href = "https://twetch.app/t/" + this.transaction;        
-}
 
 window.addEventListener('DOMContentLoaded', () => {
     const parsedUrl = new URL(window.location);

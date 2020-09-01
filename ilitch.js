d5315d98d2c487171a4c4f9a307d5972b94a836d
zeroschool <68612067+zeroschool@users.noreply.github.com>
2020-09-01 10:44:55 +0200

    Update ilitch.js

--- ilitch.js
+++ ilitch.js
@@ -230,7 +230,7 @@ async function postsQuery(){
         if (boostValue > 0){stars[i].className = 'nes-icon star is-large'};boostValues[i].innerHTML = parseInt(boostValue);
         hearts[i].addEventListener('click', like);
         stars[i].addEventListener('click', boost);
-        document.getElementById('twetch').onclick = function(){window.open("https://twetch.app/t/" + posts[i].transaction)};
+        document.getElementsByClassName('twetch').onclick = function(){window.open("https://twetch.app/t/" + posts[i].transaction)};
     }
 }
 

2c466d6eff84d718930400ec649bf905e24b1315
zeroschool <68612067+zeroschool@users.noreply.github.com>
2020-09-14 19:13:29 +0200

    Update ilitch.js

--- ilitch.js
+++ ilitch.js
@@ -304,7 +304,7 @@ function compare(a, b) {
 
 async function like() {
     document.getElementById(this.id).className = `nes-icon heart is-large`;
-    let txID = document.getElementById(this.id).slice(0,-6);
+    let txID = this.id.slice(0,-6);
     let likeCount = parseInt(document.getElementById(`${txID}_count`).innerText);
     document.getElementById(`${this.id}_count`).innerText = likeCount + 1;
     await build(this.id, 'twetch/like@0.0.1');send('twetch/like@0.0.1', this.id);
